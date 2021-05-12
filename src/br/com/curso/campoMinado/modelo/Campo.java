daf077700d8423a5eb8a34b3d3faeade71b110fc
Flavio Lima <flaviolima.s@live.com>
2021-05-12 20:13:11 -0300

    Definindo construtor

--- src/br/com/curso/campoMinado/modelo/Campo.java
+++ src/br/com/curso/campoMinado/modelo/Campo.java
@@ -5,4 +5,9 @@ public class Campo {
     private final int linha;
     private final int coluna;
     private boolean minado;
+
+    Campo(int linha, int coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
 }
