23716aa0e081c6e1a35ae0fd2a507658ef50420e
Flavio Lima <flaviolima.s@live.com>
2021-05-12 21:42:31 -0300

    Add anotation test jupter

--- test/br/com/curso/campoMinado/modelo/CampoTest.java
+++ test/br/com/curso/campoMinado/modelo/CampoTest.java
@@ -1,10 +1,17 @@
 package br.com.curso.campoMinado.modelo;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class CampoTest {
     
     private Campo campo = new Campo(3, 3);
 
+    @Test
     void testVizinhoReal() {
-
+        Campo vizinho = new Campo(3, 2);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
     }
 }
