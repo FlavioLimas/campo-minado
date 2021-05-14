5fbf2e7cfe6e7f1f7ca6df994db09abb19980e4a
Flavio Lima <flaviolima.s@live.com>
2021-05-14 00:04:59 -0300

    Definindo no cenario de testes para validar

--- test/br/com/curso/campoMinado/modelo/CampoTest.java
+++ test/br/com/curso/campoMinado/modelo/CampoTest.java
@@ -100,7 +100,7 @@ public class CampoTest {
     }
 
     @Test
-    void testAbrirComVizinhos() {
+    void testAbrirComVizinhos1() {
         Campo campo11 = new Campo(1, 1);
 
         Campo campo22 = new Campo(2, 2);
@@ -109,5 +109,20 @@ public class CampoTest {
         campo.adicionarVizinho(campo22);
         campo.abrir();
         assertTrue(campo22.isAberto() && campo11.isAberto());
+    }
+
+    @Test
+    void testAbrirComVizinhos2() {
+        Campo campo11 = new Campo(1, 1);
+        Campo campo12 = new Campo(1, 1);
+        campo12.minar();
+
+        Campo campo22 = new Campo(2, 2);
+        campo22.adicionarVizinho(campo11);
+        campo22.adicionarVizinho(campo12);
+
+        campo.adicionarVizinho(campo22);
+        campo.abrir();
+        assertTrue(campo22.isAberto() && !campo11.isAberto());
     }
 }
