ca0eec0494e17f81713cf7f5f1b5b2a65428ed57
Flavio Lima <flaviolima.s@live.com>
2021-06-09 18:16:29 -0300

    instanciando e imprimindo tabuleiro

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -1,5 +1,9 @@
+import br.com.curso.campoMinado.modelo.Tabuleiro;
+
 public class Aplicacao {
     public static void main(String[] args) {
-        
+        Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+
+        System.out.println(tabuleiro);
     }
 }
