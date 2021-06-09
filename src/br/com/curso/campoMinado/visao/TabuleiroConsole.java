4b1e4a4de18a4d4946c9d5e71242102385b3d5c2
Flavio Lima <flaviolima.s@live.com>
2021-06-09 19:33:26 -0300

    Definindo metodo

--- src/br/com/curso/campoMinado/visao/TabuleiroConsole.java
+++ src/br/com/curso/campoMinado/visao/TabuleiroConsole.java
@@ -1,12 +1,23 @@
 package br.com.curso.campoMinado.visao;
 
+import br.com.curso.campoMinado.excecao.ExitException;
 import br.com.curso.campoMinado.modelo.Tabuleiro;
 
 public class TabuleiroConsole {
-    
+
     private Tabuleiro tabuleiro;
 
     public TabuleiroConsole(Tabuleiro tabuleiro) {
         this.tabuleiro = tabuleiro;
+
+        executarGame();
+    }
+
+    private void executarGame() {
+        try {
+            
+        } catch (ExitException e) {
+
+        }
     }
 }
