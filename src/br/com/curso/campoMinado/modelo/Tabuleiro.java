6ab0f3c26e836516b886d87399a16c2d0f145ea4
Flavio Lima <flaviolima.s@live.com>
2021-06-09 12:17:05 -0300

    definindo classe e atributos

--- src/br/com/curso/campoMinado/modelo/Tabuleiro.java
+++ src/br/com/curso/campoMinado/modelo/Tabuleiro.java
@@ -1,5 +1,14 @@
 package br.com.curso.campoMinado.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+    private int linhas;
+    private int colunas;
+    private int minas;
+
+    
+
     
 }
