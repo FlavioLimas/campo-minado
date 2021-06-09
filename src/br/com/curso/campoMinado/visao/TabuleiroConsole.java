0067e614f1b92fe27c1eb9928153eb7852cd8f0b
Flavio Lima <flaviolima.s@live.com>
2021-06-09 19:36:53 -0300

    Definindo entrada e tratando close

--- src/br/com/curso/campoMinado/visao/TabuleiroConsole.java
+++ src/br/com/curso/campoMinado/visao/TabuleiroConsole.java
@@ -1,11 +1,14 @@
 package br.com.curso.campoMinado.visao;
 
+import java.util.Scanner;
+
 import br.com.curso.campoMinado.excecao.ExitException;
 import br.com.curso.campoMinado.modelo.Tabuleiro;
 
 public class TabuleiroConsole {
 
     private Tabuleiro tabuleiro;
+    private Scanner entrada = new Scanner(System.in);
 
     public TabuleiroConsole(Tabuleiro tabuleiro) {
         this.tabuleiro = tabuleiro;
@@ -18,6 +21,8 @@ public class TabuleiroConsole {
             
         } catch (ExitException e) {
 
+        } finally {
+            entrada.close();
         }
     }
 }
