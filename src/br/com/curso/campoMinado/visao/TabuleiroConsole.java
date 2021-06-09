a34fa5e3a0c7365c65d9ed24c47abb8d61329abc
Flavio Lima <flaviolima.s@live.com>
2021-06-09 20:01:25 -0300

    Definindo comportamento de execucao do game

--- src/br/com/curso/campoMinado/visao/TabuleiroConsole.java
+++ src/br/com/curso/campoMinado/visao/TabuleiroConsole.java
@@ -3,6 +3,7 @@ package br.com.curso.campoMinado.visao;
 import java.util.Scanner;
 
 import br.com.curso.campoMinado.excecao.ExitException;
+import br.com.curso.campoMinado.excecao.ExplosaoException;
 import br.com.curso.campoMinado.modelo.Tabuleiro;
 
 public class TabuleiroConsole {
@@ -18,11 +19,33 @@ public class TabuleiroConsole {
 
     private void executarGame() {
         try {
-            
+            boolean continuar = true;
+
+            while (continuar) {
+
+                startGame();
+
+                System.out.println("Outra partida? (S/n) ");
+                String resposta = entrada.nextLine();
+
+                if ("n".equalsIgnoreCase(resposta)) {
+                    continuar = false;
+                } else {
+                    tabuleiro.reiniciar();
+                }
+            }
         } catch (ExitException e) {
             System.out.println("Tchau!!!");
         } finally {
             entrada.close();
+        }
+    }
+
+    private void startGame() {
+        try {
+            System.out.println("Você ganhou!");
+        } catch (ExplosaoException e) {
+            System.out.println("Você perdeu!");
         }
     }
 }
