55d73c157874ea9caee75cbbbe32452437e9d1fc
Flavio Lima <flaviolima.s@live.com>
2021-06-09 18:06:32 -0300

    definindo instrucoes para minar campo

--- src/br/com/curso/campoMinado/modelo/Tabuleiro.java
+++ src/br/com/curso/campoMinado/modelo/Tabuleiro.java
@@ -43,6 +43,9 @@ public class Tabuleiro {
         Predicate<Campo> minado = c -> c.isMinado();
         do {
             minasArmadas = campos.stream().filter(minado).count();
+            int aleatorio = (int) (Math.random() * campos.size());
+            campos.get(aleatorio).minar();
         } while(minasArmadas < minas);
     }
+
 }
