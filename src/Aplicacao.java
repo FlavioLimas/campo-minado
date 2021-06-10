46d74bb9a90c93c92f97b42a8a2b98d21f3dac02
Flavio Lima <flaviolima.s@live.com>
2021-06-09 21:28:54 -0300

    correcoes de bugs

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -4,7 +4,7 @@ import br.com.curso.campoMinado.visao.TabuleiroConsole;
 public class Aplicacao {
     public static void main(String[] args) {
 
-        Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+        Tabuleiro tabuleiro = new Tabuleiro(6, 6, 3);
         new TabuleiroConsole(tabuleiro);
     }
 }
